bb421060655c0df770c56ed4eac437075d6afb93
jleath <lawnfurniture@icloud.com>
2017-06-05 01:35:02 +0000

    finished 32 case

--- src/trans.c
+++ src/trans.c
@@ -22,16 +22,39 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-    int curr_max = 8;
-    for (int ii = 0; ii < N; ++ii) {
-        for (int jj = 0; jj < M; ++jj) {
-            for (int i = ii; i < curr_max; ++i) {
-                for (int j = jj; jj < curr_max; ++j) {
-                    B[j][i] = A[i][j];
-                }
+    int i, j, k;
+    int block_size = 8;
+    if (N == 32) {
+        for (k = 0; k < N; k += block_size) {
+            for (i = 0; i < N; i += 1) {
+                j = k;
+                int a00 = A[i][j];
+                int a01 = A[i][j+1];
+                int a02 = A[i][j+2];
+                int a03 = A[i][j+3];
+                int a04 = A[i][j+4];
+                int a05 = A[i][j+5];
+                int a06 = A[i][j+6];
+                int a07 = A[i][j+7];
+                B[j][i] = a00;
+                B[j+1][i] = a01;
+                B[j+2][i] = a02;
+                B[j+3][i] = a03;
+                B[j+4][i] = a04;
+                B[j+5][i] = a05;
+                B[j+6][i] = a06;
+                B[j+7][i] = a07;
             }
         }
-    } 
+    }else if (N == 64) {
+        for (k = 0; k < N; k += block_size) {
+            for (i = 0; i < N; ++i) {
+                for (j = k; j < k + block_size; ++j) {
+                    B[j][i] = A[i][j]; 
+                } 
+            }
+        }
+    }
 }
 
 /* 
